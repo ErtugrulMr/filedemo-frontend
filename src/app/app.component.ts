51ceb76ca1f4f4d4c0d0b03475d6b20e823610e6
ErtugrulMr <ertugrul4653@gmail.com>
2021-07-31 11:34:34 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FileService } from './services/file.service';
+import { HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,78 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'filedemo';
+  fileNames: string[] = [];
+  fileStatus = {status: '', requestType: '', percent: 0};
+
+
+  constructor(private fileService: FileService) {}
+
+  //define a funtion to upload files
+
+  onUploadFiles(files: File[]): void{
+    const formData = new FormData();
+    for (const file of files){
+      formData.append('files', file, file.name);
+        this.fileService.upload(formData).subscribe(
+        event=>{
+          console.log(event);
+          this.reportProgress(event);
+        },
+        (error: HttpErrorResponse) =>{
+          console.error(error);
+        }
+      );
+    }
+  }
+
+  //define a funtion to download files
+
+  onDownloadFiles(fileName: string): void{
+    this.fileService.download(fileName).subscribe(
+      event=>{
+        console.log(event);
+        this.reportProgress(event);
+      },
+      (error: HttpErrorResponse) =>{
+        console.error(error);
+      }
+    )
+  }
+
+  private reportProgress(httpEvent: HttpEvent<string[] | Blob>): void{
+    switch(httpEvent.type){
+      case HttpEventType.UploadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total!, 'Uploading');
+        break;
+      case HttpEventType.DownloadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total!, 'Downloading');
+        break;
+      case HttpEventType.ResponseHeader:
+        console.log('Header returned', httpEvent);
+        break;
+      case HttpEventType.Response:
+        if(httpEvent.body instanceof Array){
+          for(const fileName of httpEvent.body){
+            if(!this.fileNames.includes(fileName)){
+              this.fileNames.unshift(fileName);
+            }
+          }
+        }
+        else{
+          saveAs(new File([httpEvent.body!], httpEvent.headers.get('File-Name')!,
+          {type: `${httpEvent.headers.get('Content-Type')};charset=utf-8`}));
+
+          // saveAs(new Blob([httpEvent.body!], {type: `${httpEvent.headers.get('Content-Type')};charset=utf-8`}),
+          // httpEvent.headers.get('File-Name'));
+        }
+        break;
+      default:
+        console.log(httpEvent);
+    }
+  }
+  private updateStatus(loaded: number, total: number, requestType: string) {
+    this.fileStatus.status = 'progress';
+    this.fileStatus.requestType = requestType;
+    this.fileStatus.percent = (Math.round((loaded / total)*100));
+  }
 }
---
+++ src/app/services/file.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+
+  private server = 'http://localhost:8080';
+
+  constructor(private httpClient: HttpClient) { }
+
+  // define function to upload files
+
+  upload(formData: FormData): Observable<HttpEvent<string[]>>{
+    return this.httpClient.post<string[]>(this.server + "/file/upload", formData, {
+      reportProgress: true,
+      observe: 'events'
+    })
+  }
+
+  // define function to download files
+
+  download(fileName: string): Observable<HttpEvent<Blob>>{
+    console.warn(fileName)
+    return this.httpClient.get(`${this.server}/file/download/${fileName}`, {
+      reportProgress: true,
+      observe: 'events',
+      responseType: 'blob'
+    })
+  }
+}
